251f8a537c1a8c33bbde27711b43cf5f78b861ad
Atomjoy <atomjoy.official@gmail.com>
2025-04-09 13:17:21 +0200

    Update contacts

---
+++ .vscode/extensions.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Get installed extensions list
+# code --list-extensions | % { "code --install-extension $_" }
+
+# Mini
+code --install-extension ahinkle.laravel-model-snippets
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
+code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension avetis.nord-palette
+code --install-extension beardedbear.beardedtheme
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension cnmorocho.min-gruvbox-theme
+code --install-extension codingyu.laravel-goto-view
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dotjoshjohnson.xml
+code --install-extension enkia.tokyo-night
+code --install-extension esbenp.prettier-vscode
+code --install-extension github.github-vscode-theme
+code --install-extension ivankinder.one-monokai-darker-spectrum
+code --install-extension liamhammett.inline-parameters
+code --install-extension lninl.theme-hbuilderx-soft-green-light
+code --install-extension moldaz.minimal-green
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension onecentlin.laravel-blade
+code --install-extension pkief.material-icon-theme
+code --install-extension sharmil.dark-room
+code --install-extension shufo.vscode-blade-formatter
+code --install-extension tobiasalthoff.atom-material-theme
+code --install-extension vue.volar
+code --install-extension wheredoesyourmindgo.gruvbox-concoctis
+code --install-extension xabikos.javascriptsnippets
+
+
+# Mid (optimal)
+code --install-extension ahinkle.laravel-model-snippets
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
+code --install-extension avetis.nord-palette
+code --install-extension beardedbear.beardedtheme
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension cnmorocho.min-gruvbox-theme
+code --install-extension codingyu.laravel-goto-view
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dotjoshjohnson.xml
+code --install-extension editorconfig.editorconfig
+code --install-extension enkia.tokyo-night
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.code-runner
+code --install-extension github.github-vscode-theme
+code --install-extension gruntfuggly.todo-tree
+code --install-extension hakcorp.php-awesome-snippets
+code --install-extension liamhammett.inline-parameters
+code --install-extension lninl.theme-hbuilderx-soft-green-light
+code --install-extension mikestead.dotenv
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension onecentlin.laravel-blade
+code --install-extension pkief.material-icon-theme
+code --install-extension rapidapi.vscode-rapidapi-client
+code --install-extension rapidapi.vscode-services
+code --install-extension shufo.vscode-blade-formatter
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vue.volar
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension wheredoesyourmindgo.gruvbox-concoctis
+code --install-extension xabikos.javascriptsnippets
+
+# Full
+code --install-extension ahinkle.laravel-model-snippets
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
+code --install-extension avetis.nord-palette
+code --install-extension beardedbear.beardedtheme
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension cnmorocho.min-gruvbox-theme
+code --install-extension codingyu.laravel-goto-view
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension devsense.composer-php-vscode
+code --install-extension devsense.intelli-php-vscode
+code --install-extension devsense.phptools-vscode
+code --install-extension devsense.profiler-php-vscode
+code --install-extension dotjoshjohnson.xml
+code --install-extension editorconfig.editorconfig
+code --install-extension enkia.tokyo-night
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.code-runner
+code --install-extension github.github-vscode-theme
+code --install-extension gruntfuggly.todo-tree
+code --install-extension hakcorp.php-awesome-snippets
+code --install-extension liamhammett.inline-parameters
+code --install-extension lninl.theme-hbuilderx-soft-green-light
+code --install-extension mikestead.dotenv
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension onecentlin.laravel-blade
+code --install-extension pkief.material-icon-theme
+code --install-extension rapidapi.vscode-rapidapi-client
+code --install-extension rapidapi.vscode-services
+code --install-extension sainnhe.gruvbox-material
+code --install-extension shufo.vscode-blade-formatter
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vue.volar
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension wheredoesyourmindgo.gruvbox-concoctis
+code --install-extension xabikos.javascriptsnippets
